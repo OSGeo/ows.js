ca1166266713af4954bbb164de428d8bd2d2d00e
juanrapoport <juanrapoport@gmail.com>
2015-04-15 15:14:03 -0300

    Fix GetRecords object

--- src/csw.js
+++ src/csw.js
@@ -167,7 +167,7 @@ Ows4js.Csw.GetRecords = function(startPosition, maxRecords, query){
         TYPE_NAME: "CSW_2_0_2.GetRecordsType",
         abstractQuery: query,
         startPosition: startPosition,
-        maxRecords: 10,
+        maxRecords: maxRecords,
         resultType: "results",
         service: "CSW",
         version: "2.0.2"
